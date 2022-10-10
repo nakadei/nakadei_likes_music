b8f1f256031dfc108af3f5c0936d20a08a259229
nakadei <umuneko@gmail.com>
2022-10-10 23:13:51 +0900

    feat: update http to https

--- api/music.ts
+++ api/music.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import { ItunesStoreMusic } from '../types/itunesStore'
 
-export function getMusic(wslId: string): Promise<ItunesStoreMusic> {
-  return axios.get(`http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsLookup?id=${wslId}&country=JP`).then(res => {
+export async function getMusic(trackId: string): Promise<ItunesStoreMusic> {
+  const url = `https://itunes.apple.com/lookup?id=${trackId}&country=JP`
+  // NOTE: 古いURLっぽい
+  //const url = `http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsLookup?id=${trackId}&country=JP`
+  return axios.get(url).then(res => {
     console.log('[API] getMusic: ', res.data.results[0])
     // results は unique id で検索しても必ず array で帰ってくるのでこうしてる
     return res.data.results[0]
